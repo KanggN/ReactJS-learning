328953122b0bcf0f86e22d19186845a7aa172264
KanggN <quangnguyenmail96@gmail.com>
2020-10-25 17:30:23 +0700

    finish showing product with array function and doing showing input value

--- test-app/src/App.js
+++ test-app/src/App.js
@@ -4,48 +4,62 @@ import Header from './components/Header'
 import Product from './components/Product'
 
 class App extends Component {
-  Showinfo(a){
-    if(a.Status){
-      return <h1>
-              Id : {a.id} <br/>
-              Ten : {a.Ten}<br/>
-              Lop : {a.Lop}<br/>
-              status : {a.Status ? 'Active' : 'Pending'}
-            </h1>
-    };
-  };
+  them = () => {
+    console.log(this.refs)
+  }
+
   render () {
-    var a = 4;
-    var b = 3;
-    var Hocsinh = {
+    var SanPhams =[ {
       id : 1,
-      Ten : 'Quang',
-      Lop : 12,
-      Status : true
-    }   
+      name : 'Iphone 6s',
+      image: 'https://fptshop.com.vn/Uploads/Originals/2020/1/7/637139916609041371_6s-pl-vang-1.png',
+      status: true
+    },
+    {
+      id : 2,
+      name: 'Samsung galaxy 10',
+      image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTlVoCou11mnMql0fEEoBaPUxB7YlYmKsAKm9pLmD_RzrOFRod8mxv6USMZ_miVB-nRvGhKwsz6&usqp=CAc',
+      status: true
+    },
+    {
+      id: 3,
+      name: 'Xiaomi12',
+      image: 'https://cdn.tgdd.vn/Products/Images/42/217308/xiaomi-redmi-9-114620-094644-400x400.jpg',
+      status: true
+    },
+    {
+      id: 4,
+      name: 'Nokia',
+      image: 'https://cdn1.viettelstore.vn/images/Product/ProductImage/medium/789274353.jpeg',
+      status: true
+    },
+  ];
+    let Holyshit = SanPhams.map((Sanpham,index)=>{
+            return <Product
+                    key={Sanpham.id}
+                    name={Sanpham.name}
+                    image={Sanpham.image}/>
+    })  
+
+
+
     return (
       <div className="App">
         <Header/>
         <div className="container">
           <div className="row mt-3">
-              <div className="col-4">
-                  <Product name="Iphone 6s"
-                            price="6000000"
-                            image="https://fptshop.com.vn/Uploads/Originals/2020/1/7/637139916609041371_6s-pl-vang-1.png" />
-              </div>
-              <div className="col-4">
-                  <Product name="Samsung galaxy 10"
-                            price="12000000" 
-                            image="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTlVoCou11mnMql0fEEoBaPUxB7YlYmKsAKm9pLmD_RzrOFRod8mxv6USMZ_miVB-nRvGhKwsz6&usqp=CAc"/>
-              </div>
-              <div className="col-4">
-                  <Product name="Xiaomi12"
-                            price="8000000" 
-                            image="https://cdn.tgdd.vn/Products/Images/42/217308/xiaomi-redmi-9-114620-094644-400x400.jpg"/>
-              </div>
+                  {Holyshit}
           </div>
-        </div>
-        
+        </div> 
+        <div className="row">
+          <div className="col-4 mx-auto">
+            <div className="form-group">
+              <label for=""></label>
+              <input type="text" name="dienthoai" ref="gido" id="" className="form-control" placeholder=""/>
+              <button type="button" className="btn btn-primary mt-3" onClick={this.Them}>Lưu</button>
+            </div>
+          </div>
+        </div> 
       </div>
     )
   }
@@ -54,29 +68,3 @@ class App extends Component {
 export default App;
 
 
-/* 
-  function Showinfo(a){
-    if(a.Status){
-      return <h1>
-              Id : {a.id} <br/>
-              Ten : {a.Ten}<br/>
-              Lop : {a.Lop}<br/>
-              status : {a.Status ? 'Active' : 'Pending'}
-            </h1>
-    };
-  };
-  function App() {
-    var Hocsinh = {
-      id : 1,
-      Ten : 'Quang',
-      Lop : 12,
-      Status : true
-    } 
-    return (
-      <div>
-        <Header/>
-        {Showinfo(Hocsinh)}
-      </div>
-  
-    )
-  } */
--- test-app/src/components/Product.js
+++ test-app/src/components/Product.js
@@ -3,45 +3,21 @@ import React, { Component } from 'react';
 
 class Product extends Component { 
     
-      
+    Ohshit = () =>{
+      console.log(this.props.name)
+    }
     render (){
-        var SanPhams =[ {
-            id = 1,
-            name = 'Iphone 6s',
-            image = 'https://fptshop.com.vn/Uploads/Originals/2020/1/7/637139916609041371_6s-pl-vang-1.png',
-            status = true
-          },
-          {
-            id = 2,
-            name = 'Samsung galaxy 10',
-            image = 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTlVoCou11mnMql0fEEoBaPUxB7YlYmKsAKm9pLmD_RzrOFRod8mxv6USMZ_miVB-nRvGhKwsz6&usqp=CAc',
-            status = true
-          },
-          {
-            id = 3,
-            name = 'Xiaomi12',
-            image = 'https://cdn.tgdd.vn/Products/Images/42/217308/xiaomi-redmi-9-114620-094644-400x400.jpg',
-            status = true
-          }]
-          let Holyshit = SanPhams.map((Sanpham,index)=>{
-                let result = '';
-                if(SanPhams.status)
-                result = <div class="card">
-                                <img class="card-img-top" src={this.props.image} alt="" />
-                                <div class="card-body">
-                                    <h4 class="card-title">{this.props.name}</h4>
-                                    <p class="card-text">{this.props.price}</p>
-                                    <a type="button" className="btn btn-primary">Mua ngay</a>
-                                </div>
-                        </div>
-                return result;
-          })
-
-         
         return (   
-                <div>
-                    {Holyshit}
-                </div>    
+                <div className="col-4 mt-3">
+                      <div className="card">
+                      <img className="card-img-top" src={this.props.image} alt=""/>
+                      <div className="card-body">
+                        <h4 className="card-title">{this.props.name}</h4>
+                        <button type="button" className="btn btn-primary" onClick={this.Ohshit}>Mua ngay</button>
+                      </div>
+                    </div> 
+                </div>
+                
         )
     }
   
