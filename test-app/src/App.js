8593afd4379e2b38ada6544a8e82a734364233d0
KanggN <quangnguyenmail96@gmail.com>
2020-10-28 12:24:56 +0700

    stuck on toggling boolin of todolist checkboxes

--- test-app/src/App.js
+++ test-app/src/App.js
@@ -10,6 +10,57 @@ class App extends React.Component {
   constructor(){
     super()
     this.state = {
+      todos : Todosdata
+    }
+    this.handleClick = this.handleClick.bind(this)
+  }
+  handleClick(id){
+    this.setState(prevState =>{
+      const updatedTodos = prevState.todos.map(todo=>{
+        if(todo.id === id) {
+          todo.completed = !todo.completed
+        }
+        return todo
+      })
+      console.log(updatedTodos)
+      return {
+        todos : updatedTodos
+      }
+    })
+  }
+  render(){
+    const abc = this.state.todos.map(item=> <TodoItem key = {item.id} item = {item} handleClick={this.handleClick}/>)
+    return(
+      <div className="Appp">
+        <div className="container">
+          {abc}
+        </div>
+      </div>
+    )
+  }
+}
+export default App
+
+
+
+/* 
+constructor(){
+  super()
+  this.state = {
+    todos : Todosdata
+  }
+}
+<div className="Todo-list">
+        <div className="container">
+        {Todoitems}
+        </div>
+      </div>
+ const Todoitems = this.state.todos.map(item=> <TodoItem key={item.id} item={item} />) */
+/* ----------------------------------------------------------------------------------------------------------------- */
+ /* class App extends React.Component {
+  constructor(){
+    super()
+    this.state = {
       count: 0
     }
     this.handleClick = this.handleClick.bind(this)
@@ -27,27 +78,37 @@ class App extends React.Component {
         <div className="container">
             <h1>{this.state.count}</h1>
             <button onClick={this.handleClick}>Change!</button>
-            <button onClick={()=>console.log(this.state.count)}>Check</button>
         </div>  
       </div>
     )
   }
 };
-export default App
+export default App */
 
 
 
+
+/* handleClick(id){
+  this.setState(prevState =>{
+    const updatedtodos = prevState.todos.map(todo =>{
+      if(todo.id === id){
+        todo.completed = !todo.completed
+      }
+        return todo
+    })
+    return {todos : updatedtodos}
+  })
+} */
 
 /* 
-constructor(){
-  super()
-  this.state = {
-    todos : Todosdata
+this.setState(prevState =>{
+  const updatedTodos = prevState.todos.map(todo=>{
+    if(todo.id === id) {
+      todo.completed = !todo.completed
+    }
+    return todo
+  })
+  return {
+    todos : updatedTodos
   }
-}
-<div className="Todo-list">
-        <div className="container">
-        {Todoitems}
-        </div>
-      </div>
- const Todoitems = this.state.todos.map(item=> <TodoItem key={item.id} item={item} />) */
+}) */
--- test-app/src/components/Todoitems.js
+++ test-app/src/components/Todoitems.js
@@ -1,16 +1,15 @@
 import React from 'react'
 
 function TodoItem(props)  {
-
-
         return(
             <div className="Todo-item">
-                <input type="checkbox" defaultChecked={props.item.completed}/>
+                <input  type="checkbox" 
+                        id={props.item.id}
+                        checked={props.item.completed}
+                        onChange={() => props.handleClick(props.item.id)}/>
                 <p>{props.item.text}</p>
-                
             </div>
         )
-
 }
 
 export default TodoItem
