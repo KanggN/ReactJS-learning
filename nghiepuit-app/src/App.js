37a409fd284f00f38706d9f32346f4092cb05200
KanggN <quangnguyenmail96@gmail.com>
2020-10-30 17:56:09 +0700

    move Colorpicker to txt file. Start form project of nghiepuit stop at input username and password

--- nghiepuit-app/src/App.js
+++ nghiepuit-app/src/App.js
@@ -1,58 +1,59 @@
 import react from 'react'
 import './App.css';
-import ColorPicker from './components/Colorpicker'
-import SizeSetting from './components/SizeSetting'
-import Reset from './components/Reset'
-import Result from './components/Result'
+
 class App extends react.Component {
   constructor(){
     super()
     this.state = {
-      color : 'red',
-      fontSize : 15
+      username : '',
+      password : ''
     }
-    this.SetColor = this.SetColor.bind(this)
-    this.SetFontSize = this.SetFontSize.bind(this)
-    this.onSettingDefault = this.onSettingDefault.bind(this)
- 
+    this.onHandleChange = this.onHandleChange.bind(this)
+    this.onHandleSubmit = this.onHandleSubmit.bind(this)
   }
-  SetColor(param){
+  onHandleChange(e){
+    var name = e.target.name;
+    var value = e.target.value;
     this.setState({
-      color : param
+      [name] : value
     })
   }
-  SetFontSize(param){
-    this.setState({
-      fontSize : param
-    })
-  }
-  onSettingDefault(value){
-    if(value)
-    this.setState({
-      color : 'red',
-      fontSize : 15
-    })
+  onHandleSubmit(e){
+    e.preventDefault()
+    console.log(this.state)
   }
   render(){
     return(
-      <div className="App">
-        <div className="container mt-5">
-          <div className="row"> 
-            <div className="col-6">
-              <ColorPicker color={this.state.color} ReceiveColor={this.SetColor}/>   
-            </div>       
-            <div className="col-6">
-                <SizeSetting fontSize={this.state.fontSize} ReceiveFontSize={this.SetFontSize}/>
-                <Reset onSettingDefault={this.onSettingDefault}/>
+      <div className="container mt-5">
+        <div className="row">
+          <div className="col-5 ml-5">
+            <div className="card border-primary">
+                <div className="card-header bg-primary">Form</div>
+                  <form onSubmit={this.onHandleSubmit}>
+                  <div className="form-group">
+                    <div className="card-body">
+                            <label>Tên</label>
+                            <input type="text"
+                              className="form-control" 
+                              name="username" 
+                              onChange={this.onHandleChange} />
+                      </div>
+                    </div>
+                    <div className="form-group">
+                    <div className="card-body">
+                            <label>Mật Khẩu</label>
+                            <input type="text"
+                              className="form-control" 
+                              name="password" 
+                              onChange={this.onHandleChange} />
+                      </div>
+                    </div>
+                    <button type="submit" className="btn btn-primary m-2">Submit</button>
+                    <button type="reset" className="btn btn-success m-2">Reset</button>
+                  </form>
             </div>
           </div>
-          <div className="row mt-5">
-          <div className="col-12">
-              <Result color = {this.state.color}
-                      fontSize = {this.state.fontSize}/>
-            </div> 
-          </div>
-        </div>
+        </div> 
       </div>
     )
   }
